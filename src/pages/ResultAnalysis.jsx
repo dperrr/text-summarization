8549a112c8ae2598edd4368528dfec9aa2a12034
dperrr <rosellonjasperdan30@gmail.com>
2025-08-13 14:40:28 +0800

    feat result analaysis

---
+++ src/pages/ResultAnalysis.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+function ResultAnalysis() {
+  return (
+    <div>
+      <h1>Result Analysis</h1>
+      <p>This is the result analysis page.</p>
+    </div>
+  );
+}
+
+export default ResultAnalysis;
